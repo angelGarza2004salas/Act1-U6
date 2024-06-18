2bb0d2a6f30d664e8383b89cc02f4fe1c5c9afc8
angelGarza2004salas <125837002+angelGarza2004salas@users.noreply.github.com>
2024-06-18 14:41:23 -0600

    Add files via upload

---
+++ AppPizza_Sales/AppPizza_Sales/Form1.Designer.vb
@@ -0,0 +1,118 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+	Inherits System.Windows.Forms.Form
+
+	'Form overrides dispose to clean up the component list.
+	<System.Diagnostics.DebuggerNonUserCode()>
+	Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+		Try
+			If disposing AndAlso components IsNot Nothing Then
+				components.Dispose()
+			End If
+		Finally
+			MyBase.Dispose(disposing)
+		End Try
+	End Sub
+
+	'Required by the Windows Form Designer
+	Private components As System.ComponentModel.IContainer
+
+	'NOTE: The following procedure is required by the Windows Form Designer
+	'It can be modified using the Windows Form Designer.  
+	'Do not modify it using the code editor.
+	<System.Diagnostics.DebuggerStepThrough()>
+	Private Sub InitializeComponent()
+		dgvDatapizzas = New DataGridView()
+		btnCalcule = New Button()
+		lbtotal = New Label()
+		btnopenfile = New Button()
+		txtdate = New TextBox()
+		Label1 = New Label()
+		CType(dgvDatapizzas, ComponentModel.ISupportInitialize).BeginInit()
+		SuspendLayout()
+		' 
+		' dgvDatapizzas
+		' 
+		dgvDatapizzas.AllowUserToDeleteRows = False
+		dgvDatapizzas.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize
+		dgvDatapizzas.Location = New Point(22, 72)
+		dgvDatapizzas.Name = "dgvDatapizzas"
+		dgvDatapizzas.ReadOnly = True
+		dgvDatapizzas.RowHeadersWidth = 51
+		dgvDatapizzas.SelectionMode = DataGridViewSelectionMode.FullRowSelect
+		dgvDatapizzas.Size = New Size(1199, 578)
+		dgvDatapizzas.TabIndex = 0
+		' 
+		' btnCalcule
+		' 
+		btnCalcule.Font = New Font("Segoe UI", 10.8F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+		btnCalcule.Location = New Point(1311, 256)
+		btnCalcule.Name = "btnCalcule"
+		btnCalcule.Size = New Size(130, 55)
+		btnCalcule.TabIndex = 2
+		btnCalcule.Text = "Calculate"
+		btnCalcule.UseVisualStyleBackColor = True
+		' 
+		' lbtotal
+		' 
+		lbtotal.AutoSize = True
+		lbtotal.Font = New Font("Segoe UI", 13.8F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+		lbtotal.Location = New Point(1266, 351)
+		lbtotal.Name = "lbtotal"
+		lbtotal.Size = New Size(0, 31)
+		lbtotal.TabIndex = 3
+		' 
+		' btnopenfile
+		' 
+		btnopenfile.Font = New Font("Segoe UI", 10.8F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+		btnopenfile.Location = New Point(22, 12)
+		btnopenfile.Name = "btnopenfile"
+		btnopenfile.Size = New Size(130, 55)
+		btnopenfile.TabIndex = 4
+		btnopenfile.Text = "Open File"
+		btnopenfile.UseVisualStyleBackColor = True
+		' 
+		' txtdate
+		' 
+		txtdate.Font = New Font("Segoe UI", 15.0F)
+		txtdate.Location = New Point(1277, 197)
+		txtdate.Name = "txtdate"
+		txtdate.Size = New Size(206, 41)
+		txtdate.TabIndex = 5
+		' 
+		' Label1
+		' 
+		Label1.AutoSize = True
+		Label1.Font = New Font("Segoe UI", 13.8F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+		Label1.Location = New Point(1277, 132)
+		Label1.Name = "Label1"
+		Label1.Size = New Size(197, 62)
+		Label1.TabIndex = 6
+		Label1.Text = "Format to enter " & vbCrLf & ChrW(8220) & "year-month-day" & ChrW(8221)
+		' 
+		' Form1
+		' 
+		AutoScaleDimensions = New SizeF(8.0F, 20.0F)
+		AutoScaleMode = AutoScaleMode.Font
+		ClientSize = New Size(1555, 673)
+		Controls.Add(Label1)
+		Controls.Add(txtdate)
+		Controls.Add(btnopenfile)
+		Controls.Add(lbtotal)
+		Controls.Add(btnCalcule)
+		Controls.Add(dgvDatapizzas)
+		Name = "Form1"
+		Text = "Form1"
+		CType(dgvDatapizzas, ComponentModel.ISupportInitialize).EndInit()
+		ResumeLayout(False)
+		PerformLayout()
+	End Sub
+
+	Friend WithEvents dgvDatapizzas As DataGridView
+	Friend WithEvents btnCalcule As Button
+	Friend WithEvents lbtotal As Label
+	Friend WithEvents btnopenfile As Button
+	Friend WithEvents txtdate As TextBox
+	Friend WithEvents Label1 As Label
+
+End Class
---
+++ AppPizza_Sales/AppPizza_Sales/Form1.vb
@@ -0,0 +1,73 @@
+﻿Imports System.IO
+
+Public Class Form1
+	Dim filePath As String
+	Dim total As Decimal
+	Public Sub New()
+		InitializeComponent()
+		total = 0
+
+	End Sub
+	Private Sub btnopenfile_Click(sender As Object, e As EventArgs) Handles btnopenfile.Click
+		Dim openFileDialog As New OpenFileDialog()
+		openFileDialog.Filter = "Text Files (*.txt)|*.txt|All Files (*.*)|*.*"
+		openFileDialog.Title = "Open a Text File"
+		openFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments)
+		If openFileDialog.ShowDialog() <> DialogResult.OK Then
+			MessageBox.Show("A file was not selected")
+			Return
+		End If
+		filePath = openFileDialog.FileName
+		Try
+			Dim lines As String() = File.ReadAllLines(filePath)
+			Dim dt As New DataTable()
+			Dim headers As String() = lines(0).Split(","c)
+			For Each header As String In headers
+				dt.Columns.Add(header)
+			Next
+
+			For i As Integer = 1 To lines.Length - 1
+				Dim row As String() = lines(i).Split(","c)
+				dt.Rows.Add(row)
+
+			Next
+			' Agregar una nueva columna decimal para almacenar los valores de la columna 8.
+			dt.Columns.Add("Price", GetType(Decimal))
+
+			For Each dr As DataRow In dt.Rows
+				Dim priceString As String = dr(8).ToString()
+				Dim priceDecimal As Decimal = 0
+				Decimal.TryParse(priceString, priceDecimal)
+				dr("Price") = priceDecimal
+			Next
+
+			dt.Columns.RemoveAt(8)
+			dgvDatapizzas.DataSource = dt
+
+			MessageBox.Show("File loaded successfully.", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information)
+		Catch ex As Exception
+			MessageBox.Show("An error occurred while loading the file. " & ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+		End Try
+
+
+
+	End Sub
+
+	Private Sub btnCalcule_Click(sender As Object, e As EventArgs) Handles btnCalcule.Click
+
+		Dim _date As String = txtdate.Text
+		Dim precioTotal As Decimal = 0
+
+		For Each fila As DataGridViewRow In dgvDatapizzas.Rows
+			Dim daterow As String = fila.Cells(3).Value
+
+			If daterow = _date Then
+				Dim precio As Decimal = fila.Cells(8).Value
+				precioTotal += precio
+			End If
+		Next
+		lbtotal.Text = "The total is: " & precioTotal.ToString("0.00")
+
+
+	End Sub
+End Class
